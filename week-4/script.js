5d9a47352119937ad62d4f371bd2043dee46bb7b
Umang2000 <umangr2000@gmail.com>
2020-07-11 10:33:06 +0530

    Update script.js

--- week-4/script.js
+++ week-4/script.js
@@ -35,7 +35,7 @@ WARNING!!! WARNING!!!
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
 
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Umang", "Frank", "Larry", "Paula", "Laura", "Jim", "Johny"];
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 // STEP 10:
 // Loop over the names array and say either 'Hello' or "Good Bye"
